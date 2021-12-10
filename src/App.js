931487c112c4e81531fbaa54730575298993a716
Netia Ingram <c-netia.ingram@charter.com>
2021-12-10 12:35:03 -0700

    Add tasks component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { useState } from 'react';
 import './index.css';
-import Header from './components/Header'
+import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Learn React',
+      day: 'Friday',
+      reminder: true
+    },
+    {
+      id: 2,
+      text: 'Add React to Raven',
+      day: 'Saturday',
+      reminder: false
+    }
+  ])
+
   return (
     <div className="App">
       <Header user='Thomas'/>
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
